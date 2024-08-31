b9a54f319a1afe1ba9b894a726d3a79ad68b68f1
CodingWithJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2024-08-31 17:08:12 -0400

    updates

--- src/commands/insert.rs
+++ src/commands/insert.rs
@@ -1,9 +1,16 @@
 use futures::future::{BoxFuture, FutureExt};
 
-use crate::protocol::Database;
+use crate::protocol::{Database, DbValue};
 
 // todo return errors on bad input vs using default values
-pub fn insert_command(_args: Vec<String>, _db: Database) -> BoxFuture<'static, String>
+pub fn insert_command(args: Vec<String>, db: Database) -> BoxFuture<'static, String>
 {
-    async move { todo!("Implement insert command") }.boxed()
+    async move {
+        let key = &args[0];
+        let value = &args[1];
+
+        db.write().await.insert(key.to_string(), DbValue::Text(value.to_owned()));
+        "OK".to_string()
+    }
+    .boxed()
 }
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -13,7 +13,7 @@ pub async fn handler(command_str: String, db: Database) -> String
         return "Invalid command".to_string();
     }
 
-    let command_name = parts[0].to_uppercase();
+    let command_name: String = parts[0].to_uppercase();
     let args: Vec<String> = parts[1..].iter().map(|&s| s.to_string()).collect();
 
     if let Some(command) = COMMANDS.get(command_name.as_str()) {
